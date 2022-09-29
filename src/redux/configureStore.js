1fd2628f43fb7c4f57650165ae93121371f7a3cb
benardop <benard.oc.p@gmail.com>
2022-09-30 01:03:57 +0300

    Added redux configure.js script file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -6,6 +6,13 @@ const rootReducer = combineReducers({
   books: bookReducer,
   categories: catagoriesReducer,
 });
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: ['books/getAllBooks//fulfilled', 'books/removeBook//fulfilled', 'books/addBook/fulfilled'],
+    },
+  }),
+});
 
 export default store;
